614dfe66dbae1f58dc8cf06567213445a03c4061
sbrevolution5 <sbrevolution5@aim.com>
2021-07-12 13:20:05 -0400

    shopping migration

---
+++ MasterMeal/Migrations/20210712171949_004-shoppingListNameandpantry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterMeal.Migrations
+{
+    public partial class _004shoppingListNameandpantry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
