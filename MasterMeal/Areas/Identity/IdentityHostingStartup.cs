c7a2968881eff9cae43a743bb948d3959d759734
sbrevolution5 <sbrevolution5@aim.com>
2021-07-22 14:57:51 -0400

    Id scaffold

---
+++ MasterMeal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MasterMeal.Data;
+using MasterMeal.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MasterMeal.Areas.Identity.IdentityHostingStartup))]
+namespace MasterMeal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
