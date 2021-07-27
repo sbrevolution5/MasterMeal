660bf92209ddf8184e4d11c8392c394ae598d1b7
sbrevolution5 <sbrevolution5@aim.com>
2021-07-27 13:34:11 -0400

    Adding codebehindFiles

--- MasterMealBlazor/Components/RecipeComponents/RecipeCreate.razor.cs
+++ MasterMealBlazor/Components/RecipeComponents/RecipeCreate.razor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MasterMealBlazor.Models;
+using MasterMealBlazor.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,72 @@ namespace MasterMealBlazor.Components.RecipeComponents
 {
     public partial class RecipeCreate
     {
+    private RecipeType[] Types;
+        protected async override Task OnInitializedAsync()
+        {
+            using (var context = ContextFactory.CreateDbContext())
+            {
+                Types = await context.RecipeType.ToArrayAsync();
+            }
+
+
+
+        }
+        private List<Step> steps = new();
+        private List<string> supplies = new();
+        private List<string> ingredients = new();
+        private string recipeName;
+        private string recipeDescription;
+        private int cookingTime;
+        private int TypeId;
+        private Recipe recipe = new();
+        public async void SaveRecipe()
+        {
+            recipe.Name = recipeName;
+            recipe.Description = recipeDescription;
+            recipe.CookingTime = cookingTime;
+            recipe.TypeId = TypeId;
+            using (var context = ContextFactory.CreateDbContext())
+            {
+                context.Add(recipe);
+                await context.SaveChangesAsync();
+                foreach (var step in steps)
+                {
+                    step.RecipeId = recipe.Id;
+                    SaveStep(context, step);
+                }
+                await context.SaveChangesAsync();
+            }
+
+        }
+        private void SaveStep(DbContext context, Step step)
+        {
+            context.Add(step);
+        }
+        public void AddStep()
+        {
+            steps.Add(new Models.Step());
+        }
+        public void AddSupply()
+        {
+            supplies.Add("");
+        }
+        public void AddIngredient()
+        {
+            ingredients.Add("");
+        }
+        public void RemoveIngredient(string i)
+        {
+            ingredients.Remove(i);
+        }
+        public void RemoveSupply(string i)
+        {
+            supplies.Remove(i);
+        }
+        public void RemoveStep(Step i)
+        {
+            steps.Remove(i);
+        }
     }
+
 }
